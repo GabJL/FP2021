1da0557d8a60fab78d9a8f0de43563722840c4c9
Gabriel Luque <GabJL@users.noreply.github.com>
2021-09-28 15:25:51 +0200

    Create t2e03b.py

---
+++ temas/tema1/codigo/t2e03b.py
@@ -0,0 +1,17 @@
+from turtle import *
+
+print("Dibujando un pentágono")
+
+lado = 5
+angulo = 360 / 5
+
+forward(80)
+left(angulo)
+forward(80)
+left(angulo)
+forward(80)
+left(angulo)
+forward(80)
+left(angulo)
+forward(80)
+left(angulo)
