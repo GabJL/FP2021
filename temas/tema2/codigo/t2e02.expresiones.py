e004f71b71eaee2baaade3b750125f2d1b23e6fa
Gabriel Luque <GabJL@users.noreply.github.com>
2021-09-29 12:48:19 +0200

    Create t2e02.expresiones.py

---
+++ temas/tema2/codigo/t2e02.expresiones.py
@@ -0,0 +1,15 @@
+n = 2
+numero = 3
+x = 4
+y = 2
+x1 = 1
+y1 = 0
+x2 = 0
+y2 = -1
+print("Resultados de las expresiones utilizando los valores indicados en el enuciado:")
+print("Expresión 1:", 3**7)
+print("Expresión 2:", (2*n)**0.5)
+print("Expresión 3:", 20**(-7*numero))
+e = 2.72
+print("Expresión 4:", (7*x)/(e**(20*y)))
+print("Expresión 5:", ((x1-x2)**2 + (y1-y2)**2)**0.5)
