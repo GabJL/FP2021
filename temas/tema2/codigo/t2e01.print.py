a596ea64a23a7d58b528936d6c0bd8bdaa37f70f
Gabriel Luque <GabJL@users.noreply.github.com>
2021-09-30 16:02:31 +0200

    Update t2e01.print.py

--- temas/tema2/codigo/t2e01.print.py
+++ temas/tema2/codigo/t2e01.print.py
@@ -2,4 +2,4 @@ print("Hola Gabriel!", end="\n\n")
 print("Este es mi primer programa en Python ... de este tema", end="\n\n")
 horas = 1
 minutos = 13
-print("Han pasado", horas*60 + minutos,"segundos desde el inicio de clase", end="\n\n")
+print("Han pasado", horas*3600 + minutos*60,"segundos desde el inicio de clase", end="\n\n")
