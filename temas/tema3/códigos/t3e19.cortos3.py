ff70191e3c0fc656790b94297c4ca3d74fc1cc26
Gabriel Luque <GabJL@users.noreply.github.com>
2021-10-18 16:07:27 +0200

    Create t3e19.cortos3.py

---
+++ temas/tema3/códigos/t3e19.cortos3.py
@@ -0,0 +1,12 @@
+MAX = 10 # Valores a leer para las pruebas se puede bajar a algo más manejable (3 o 4)
+
+suma = 0 # No hemos leído ningún 0
+
+num_veces = 0
+
+while num_veces < MAX:
+  num = int(input("Dime un número: "))
+  suma += num
+  num_veces += 1
+
+print("La media vale", suma/10)
