b7f92aadaef0be26c197925b8015428142604faf
Gabriel Luque <GabJL@users.noreply.github.com>
2021-10-18 16:12:33 +0200

    Create t3e20.cortos4.py

---
+++ temas/tema3/códigos/t3e20.cortos4.py
@@ -0,0 +1,16 @@
+MAX = 20 # Valores a leer para las pruebas se puede bajar a algo más manejable (3 o 4)
+
+pares = 0 
+impares = 0
+
+num_veces = 0
+
+while num_veces < MAX:
+  num = int(input("Dime un número: "))
+  if num%2 ==  0:
+    pares += 1
+  else:
+    impares += 1
+  num_veces += 1
+
+print("Se han leído", pares, "números pares y", impares, " números impares")
