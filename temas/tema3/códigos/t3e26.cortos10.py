0204953cb818dd43ed24e3cd750d7e8d6ba53fe3
Gabriel Luque <GabJL@users.noreply.github.com>
2021-10-20 16:30:27 +0200

    Create t3e26.cortos10.py

---
+++ temas/tema3/códigos/t3e26.cortos10.py
@@ -0,0 +1,12 @@
+n = int(input("Dime un número: "))
+mayor = n
+
+while n < 0:
+    if n > mayor:
+        mayor = n
+    n = int(input("Dime un número: "))
+    
+if mayor >= 0:
+    print("No se leyeron números válidos")
+else:
+    print("El mayor valor leído fue", mayor)
