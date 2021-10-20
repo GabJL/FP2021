2c25eeb141c34306733975f40161052e7bbb8403
Gabriel Luque <GabJL@users.noreply.github.com>
2021-10-20 16:05:23 +0200

    Create t3e24.cortos8.py

---
+++ temas/tema3/códigos/t3e24.cortos8.py
@@ -0,0 +1,16 @@
+MAX = 20
+
+hay_vocales = False # Aún no hemos encontrado ninguna vocal
+
+num_veces = 0
+
+while num_veces < MAX and not hay_vocales:
+    letra = input("Dime una letra: ")
+    if letra == "a" or letra == "e" or letra == "i" or letra == "o" or letra == "u":
+        hay_vocales = True
+    num_veces += 1
+
+if hay_vocales:
+    print("Se ha leído alguna vocal")
+else:
+    print("No había vocales en el texto")
