8453e601f64258c15559da3f06102be28d08c302
Gabriel Luque <GabJL@users.noreply.github.com>
2021-10-26 12:23:16 +0200

    Create p3e05.figura1.py

---
+++ prácticas/práctica3/códigos/p3e05.figura1.py
@@ -0,0 +1,18 @@
+n = int(input("Dime la altura: "))
+
+num_espacios = n - 1
+num_asteriscos = 1
+
+for linea in range(n):
+    # Dibujamos la línea:
+    # Primero espacios
+    for i in range(num_espacios):
+        print(" ", end="")
+    # Luego asteriscos
+    for i in range(num_asteriscos):
+        print("*", end= "")
+    # Y acabamos con un salto de línea
+    print()
+    # Ahora actualizamos los espacios y asteriscos para la siguiente línea
+    num_espacios -= 1
+    num_asteriscos += 2
