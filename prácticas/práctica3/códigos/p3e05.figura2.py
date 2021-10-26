2b53d2dd3929ff048b01166177606d870552239f
Gabriel Luque <GabJL@users.noreply.github.com>
2021-10-26 12:24:41 +0200

    Create p3e05.figura2.py

---
+++ prácticas/práctica3/códigos/p3e05.figura2.py
@@ -0,0 +1,37 @@
+n = int(input("Dime la altura: "))
+
+num_espacios = n - 1
+num_asteriscos = 1
+
+# Triángulo superior
+for linea in range(n):
+    # Dibujamos la línea:
+    # Primero espacios
+    for i in range(num_espacios):
+        print(" ", end="")
+    # Luego asteriscos
+    for i in range(num_asteriscos):
+        print("*", end= "")
+    # Y acabamos con un salto de línea
+    print()
+    # Ahora actualizamos los espacios y asteriscos para la siguiente línea
+    num_espacios -= 1
+    num_asteriscos += 2
+    
+# Triángulo superior
+num_espacios += 2 
+num_asteriscos -= 4
+
+for linea in range(n):
+    # Dibujamos la línea:
+    # Primero espacios
+    for i in range(num_espacios):
+        print(" ", end="")
+    # Luego asteriscos
+    for i in range(num_asteriscos):
+        print("*", end= "")
+    # Y acabamos con un salto de línea
+    print()
+    # Ahora actualizamos los espacios y asteriscos para la siguiente línea
+    num_espacios += 1
+    num_asteriscos -= 2
