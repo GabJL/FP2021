e13c9e206a2beeeebf20fe0a68e9b16bdd5e527b
Gabriel Luque <GabJL@users.noreply.github.com>
2021-11-01 21:09:35 +0100

    Create p4e02.primo.py

---
+++ prácticas/práctica4/códigos/p4e02.primo.py
@@ -0,0 +1,11 @@
+n = int(input("Introduce un número natural: "))
+
+i = 2
+while i < n and n%i != 0:
+    i += 1
+
+print("El número",n, end=" ")
+if i >= n:
+    print("es primo")
+else:
+    print("no es primo")
