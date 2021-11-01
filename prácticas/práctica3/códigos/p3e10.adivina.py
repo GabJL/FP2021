2c531549044037489fd5e9d48eedd9e69e7b09c5
Gabriel Luque <GabJL@users.noreply.github.com>
2021-11-01 18:48:59 +0100

    Create p3e10.adivina.py

---
+++ prácticas/práctica3/códigos/p3e10.adivina.py
@@ -0,0 +1,24 @@
+import random
+
+print("El ordenador está pensando un número del 1 al 100...")
+
+num_secreto = random.randint(1,100)
+
+MAX_INTENTOS = 5
+intentos = 0
+n = 0
+
+while intentos < MAX_INTENTOS and n != num_secreto:
+    n = int(input("¿Cuál cree que es el número? "))
+    intentos += 1
+    if n != num_secreto:
+        if n<num_secreto:
+            print("Lo siento, el número era mayor")
+        else:
+            print("Lo siento el número era menor")
+
+if n==num_secreto:
+    print("¡Enhorabuena, ha acertado!")
+else:
+    print("Has perdido, el número era:", num_secreto)
+    
