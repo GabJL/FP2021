ab573fe4efb9bcf47594f9fb2fc49a21faa30798
Gabriel Luque <GabJL@users.noreply.github.com>
2021-11-03 21:43:48 +0100

    Create t4e05.cilindro.py

---
+++ temas/tema4/códigos/t4e05.cilindro.py
@@ -0,0 +1,19 @@
+import math
+
+def área_círculo(radio):
+    resultado = math.pi * radio * radio
+    return resultado
+
+def longitud_círculo(radio):
+    return 2 * math.pi * radio
+
+def área_rectángulo(base, altura):
+    return base * altura
+
+def área_cilindro(radio, altura):
+    return 2 * área_círculo(radio) + área_rectángulo(longitud_círculo(radio), altura)
+
+r = float(input("Indique el radio: "))
+h = float(input("Indique la altura: "))
+
+print("El área del cilindro es", área_cilindro(r,h))
