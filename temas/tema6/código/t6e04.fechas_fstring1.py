2dad43995758da685ea7e801df513ebfd89e124d
Gabriel Luque <GabJL@users.noreply.github.com>
2021-11-29 10:29:24 +0100

    Create t6e04.fechas_fstring1.py

---
+++ temas/tema6/código/t6e04.fechas_fstring1.py
@@ -0,0 +1,8 @@
+def convertir_fecha(texto):
+  año, mes, dia = texto.split("-")
+  return f"{dia}/{mes}/{año}"
+
+# ----- PROGRAMA_PRINCIPAL -----------
+fecha = input("Dame una fecha (formato estándar): ")
+fecha_español = convertir_fecha(fecha)
+print("La fecha", fecha, "en español sería", fecha_español)
