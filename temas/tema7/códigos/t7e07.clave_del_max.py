a7a568400d7c2acae11baa2010cdfcc333b5446f
Gabriel Luque <GabJL@users.noreply.github.com>
2021-12-20 12:51:52 +0100

    Create t7e07.clave_del_max.py

---
+++ temas/tema7/códigos/t7e07.clave_del_max.py
@@ -0,0 +1,15 @@
+def max_key_dict(d):
+    maximo = list(d)[0]
+    for k in d:
+        if d[k] > d[maximo]:
+            maximo = k
+    return maximo
+
+# Programa principal
+
+Málaga = {"enero": 12, "febrero": 12, "marzo": 14, "abril": 16, "mayo": 19, "junio": 23, "julio": 26, "agosto": 26,"septiembre": 23, "octubre": 19, "noviembre": 15, "diciembre": 13}
+Yakustk = {"enero": -38, "febrero": -34, "marzo": -20, "abril": -5, "mayo": 8, "junio": 16, "julio": 20, "agosto": 15, "septiembre": 6, "octubre": -8, "noviembre": -27, "diciembre": -37}
+
+print("Málaga:", max_key_dict(Málaga))
+print("Yakustk:", max_key_dict(Yakustk))
+
