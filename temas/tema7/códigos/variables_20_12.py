54c7ab198142c9b6476ea086aa04879103bebf09
Gabriel Luque <GabJL@users.noreply.github.com>
2021-12-20 09:08:22 +0100

    Create variables_20_12.py

---
+++ temas/tema7/códigos/variables_20_12.py
@@ -0,0 +1,17 @@
+# Ejercicio 1 y 2
+
+Málaga = {"enero": 12, "febrero": 12, "marzo": 14, "abril": 16, "mayo": 19, "junio": 23, "julio": 26, "agosto": 26,"septiembre": 23, "octubre": 19, "noviembre": 15, "diciembre": 13}
+
+Yakustk = {"enero": -38, "febrero": -34, "marzo": -20, "abril": -5, "mayo": 8, "junio": 16, "julio": 20, "agosto": 15, "septiembre": 6, "octubre": -8, "noviembre": -27, "diciembre": -37}
+
+# Ejercicio 3
+Salud = {
+  "Bioquímica estructural": [70.37, 95.06, 100, 83.87], 
+  "Cálculo": [36.76, 40.91, 10.91, 36], 
+  "Fundamentos de la programación": [87.5, 78.08, 74.63, 58.57], 
+  "Física I": [51.72, 48.75, 48.45, 61.82], 
+  "Álgebra lineal": [66.27, 68.67, 49.28, 57.45]
+}
+
+# Ejercicio 4
+peli = "The Dark Knight#Action, Crime, Drama#Christopher Nolan#Christian Bale, Heath Ledger, Aaron Eckhart, Michael Caine#2008 152 2341261 9.0 84.0 534.86"
